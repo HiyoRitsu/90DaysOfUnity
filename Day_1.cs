95044ab6eddfcfdb2ee4c863ef9e7277352b9f9a
Akzhol <122852098+HiyoRitsu@users.noreply.github.com>
2023-02-21 02:15:30 +0600

    Add files via upload

---
+++ Day_1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ /*           string name = "Yato";
+            Console.WriteLine("Hello!");
+            Console.Write("Hello ");
+            Console.Write(name );
+            int x = 5, y = 10, z = 20;
+            Console.WriteLine(z + y + x);
+
+
+            int myInt = 10;
+            double myDouble = 5.25;
+            bool myBool = true;
+
+            Console.WriteLine(Convert.ToString(myInt));    // convert int to string
+            Console.WriteLine(Convert.ToDouble(myInt));    // convert int to double
+            Console.WriteLine(Convert.ToInt32(myDouble));  // convert double to int
+            Console.WriteLine(Convert.ToString(myBool));   // convert bool to string
+
+
+            string nick = Console.ReadLine();
+            Console.WriteLine("Hello " + nick);
+
+            string surname = Console.ReadLine();
+            Console.WriteLine("My surname is " + surname);
+
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("my age is " + age);
+
+            Random random= new Random();
+            double num_1 = random.NextDouble();
+            Console.WriteLine(num_1);
+ */
+        }
+    }
+}
